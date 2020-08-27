81ca88739e7e83b372b0c96c643530b3f5ca91b5
yafanisonya <yafanison@gmail.com>
2020-08-27 23:07:19 +0800

    底部导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -8,35 +8,64 @@ import {
 } from 'react-router-dom';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+ height:100vh;
+ display: flex;
+ flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Nav = styled.nav`
+  border:1px solid blue;
+  >ul{
+    display: flex;
+    >li{
+      width: 33.333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
+
 function App() {
   return (
       <Router>
-        <Switch>
-          <Route path="/tags">
-            <Tags/>
-          </Route>
-          <Route path="/money">
-            <Money/>
-          </Route>
-          <Route path="/statistics">
-            <Statistics/>
-          </Route>
-          <Redirect exact from="/" to={"money"}/>
-          <Route path="*">
-            <NoMatch/>
-          </Route>
-        </Switch>
-        <ul>
-          <li>
-            <Link to="/tags">标签页</Link>
-          </li>
-          <li>
-            <Link to="/money">记账页</Link>
-          </li>
-          <li>
-            <Link to="/statistics">统计页</Link>
-          </li>
-        </ul>
+        <Wrapper>
+          <Main>
+            <Switch>
+              <Route path="/tags">
+                <Tags/>
+              </Route>
+              <Route path="/money">
+                <Money/>
+              </Route>
+              <Route path="/statistics">
+                <Statistics/>
+              </Route>
+              <Redirect exact from="/" to={"money"}/>
+              <Route path="*">
+                <NoMatch/>
+              </Route>
+            </Switch>
+          </Main>
+          <Nav>
+            <ul>
+              <li>
+                <Link to="/tags">标签页</Link>
+              </li>
+              <li>
+                <Link to="/money">记账页</Link>
+              </li>
+              <li>
+                <Link to="/statistics">统计页</Link>
+              </li>
+            </ul>
+          </Nav>
+        </Wrapper>
       </Router>
   );
 }
