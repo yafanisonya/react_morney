ac87e7ff62494e1e39f1762766136ba835d61ce7
yafanisonya <yafanison@gmail.com>
2020-08-28 22:37:02 +0800

    添加底部导航icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react'
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -10,7 +13,15 @@ const NavWrapper = styled.nav`
     > li{
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      padding:4px 0;
+      justify-content: center;
+      align-items: center;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }      
     }
   }
 `
@@ -19,9 +30,18 @@ const Nav = () =>{
   return(
     <NavWrapper>
       <ul>
-        <li><Link to='/tags'>标签页</Link></li>
-        <li><Link to='/money'>记账页</Link></li>
-        <li><Link to='/statistics'>统计页</Link></li>
+        <li>
+          <svg className='icon'><use xlinkHref='#label'/></svg>
+          <Link to='/tags'>标签页</Link>
+        </li>
+        <li>
+          <svg className='icon'><use xlinkHref='#money'/></svg>
+          <Link to='/money'>记账页</Link>
+        </li>
+        <li>
+          <svg className='icon'><use xlinkHref='#statistics'/></svg>
+          <Link to='/statistics'>统计页</Link>
+        </li>
       </ul>
     </NavWrapper>
   )
