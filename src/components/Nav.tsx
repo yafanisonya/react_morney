3d68e91d4d76fe4870edf2a01424deb6205cb71c
yafanisonya <yafanison@gmail.com>
2020-09-02 22:43:38 +0800

    封装Icon组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,50 +1,48 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import React from 'react'
-require('icons/money.svg');
-require('icons/label.svg');
-require('icons/statistics.svg');
+import { Link } from 'react-router-dom';
+import React from 'react';
+import Icon from './Icon';
 
 const NavWrapper = styled.nav`
-  line-height: 24px;
-  box-shadow:0 0 3px rgba(0,0,0,0.25);
-  > ul{
-    display: flex;
-    > li{
-      width: 33.3333%;
-      text-align: center;
-      display: flex;
-      flex-direction: column;
-      padding:4px 0;
-      justify-content: center;
-      align-items: center;
-      .icon{
-        width: 24px;
-        height: 24px;
-      }      
-    }
-  }
-`
+	line-height: 24px;
+	box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+	> ul {
+		display: flex;
+		> li {
+			width: 33.3333%;
+			text-align: center;
+			display: flex;
+			flex-direction: column;
+			padding: 4px 0;
+			justify-content: center;
+			align-items: center;
+			.icon {
+				width: 24px;
+				height: 24px;
+			}
+		}
+	}
+`;
 
-const Nav = () =>{
-  return(
-    <NavWrapper>
-      <ul>
-        <li>
-          <svg className='icon'><use xlinkHref='#label'/></svg>
-          <Link to='/tags'>标签页</Link>
-        </li>
-        <li>
-          <svg className='icon'><use xlinkHref='#money'/></svg>
-          <Link to='/money'>记账页</Link>
-        </li>
-        <li>
-          <svg className='icon'><use xlinkHref='#statistics'/></svg>
-          <Link to='/statistics'>统计页</Link>
-        </li>
-      </ul>
-    </NavWrapper>
-  )
-}
+const Nav = () => {
+	return (
+		<NavWrapper>
+			<ul>
+				<li>
+					<Icon name="tag" />
+					<Link to="/tags">标签页</Link>
+				</li>
+				<li>
+					<Icon name="money" />
+					<Link to="/money">记账页</Link>
+				</li>
+				<li>
+					<Icon name="statistics" />
+					<Link to="/statistics">统计页</Link>
+				</li>
+			</ul>
+		</NavWrapper>
+	);
+};
 
 export default Nav;
