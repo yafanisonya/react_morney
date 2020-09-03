2afb6c361d501dab84f0c18b0504d2abff4106ff
yafanisonya <yafanison@gmail.com>
2020-09-03 22:59:59 +0800

    抽离function

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import {
 	Route,
 	Redirect,
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from 'components/Nav';
-import Layout from 'components/Layout';
-
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NoMatch';
 function App() {
 	return (
 		<Router>
@@ -28,34 +28,6 @@ function App() {
 				</Route>
 			</Switch>
 		</Router>
-	);
-}
-
-function NoMatch() {
-	return <div>页面不存在</div>;
-}
-
-function Statistics() {
-	return (
-		<Layout>
-			<h2>统计页面</h2>
-		</Layout>
-	);
-}
-
-function Tags() {
-	return (
-		<Layout>
-			<h2>标签页面</h2>
-		</Layout>
-	);
-}
-
-function Money() {
-	return (
-		<Layout>
-			<h2>记账页面</h2>
-		</Layout>
 	);
 }
 
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NoMatch() {
+	return <div>页面不存在！</div>;
+}
+
+export default NoMatch;
