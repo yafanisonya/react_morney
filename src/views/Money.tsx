093f38b373bdb6d8d8eac2fbaed6d5acdcc9fb6a
fanison <308955874@qq.com>
2021-07-06 00:06:47 +0800

    type

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -46,7 +46,27 @@ const NotesSection = styled.section`
 	}
 `;
 const CategorySection = styled.section`
-
+	font-size:24px;
+	> ul{
+		display:flex;
+		background:#c4c4c4;
+		> li{
+			position:relative;
+			padding: 16px 0;
+			text-align:center;
+			width:50%;
+			&.selected::after{
+				position:absolute;
+				bottom:0;
+				left:0;
+				display:flex;
+				content:'';
+				width:100%;
+				height:3px;
+				background:#333;
+			}
+		}
+	}
 `;
 const NumberPadSection = styled.section`
 
@@ -72,7 +92,7 @@ function Money() {
 			</NotesSection>
 			<CategorySection>
 				<ul>
-					<li>支出</li>
+					<li className="selected">支出</li>
 					<li>收入</li>
 				</ul>
 			</CategorySection>
