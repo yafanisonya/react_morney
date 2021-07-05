addf1a34b955f580c7f24da401bb7acf4992de98
fanison <308955874@qq.com>
2021-07-05 22:49:57 +0800

    tagSection ui 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,83 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import styled from 'styled-components';
+
+const TagsSection = styled.section`
+	padding:12px 16px;
+	background:#fff;
+	> ol{
+		margin:0 --12px;
+		>li{
+			display:inline-block;
+			margin:8px 12px;
+			padding: 3px 18px;
+			border-radius:18px;
+			font-size:14px;
+			background:#D9D9D9;
+		}
+	}
+	> button{
+		margin-top: 8px;
+		padding:2px 4px;
+		border:none;
+		border-bottom:1px solid #333;
+		color:#666;
+		background:none;
+	}
+`;
+const NotesSection = styled.section`
+
+`;
+const CategorySection = styled.section`
+
+`;
+const NumberPadSection = styled.section`
+
+`;
 
 function Money() {
 	return (
 		<Layout>
-			<h2>记账页面</h2>
+			<TagsSection>
+				<ol>
+					<li>衣</li>
+					<li>食</li>
+					<li>住</li>
+					<li>行</li>
+				</ol>
+				<button>新增标签</button>
+			</TagsSection>
+			<NotesSection>
+				<label>
+					<span>备注</span>
+					<input type="text"></input>
+				</label>
+			</NotesSection>
+			<CategorySection>
+				<ul>
+					<li>支出</li>
+					<li>收入</li>
+				</ul>
+			</CategorySection>
+			<NumberPadSection>
+				<div>1000</div>
+				<div>
+					<button>1</button>
+					<button>2</button>
+					<button>3</button>
+					<button>删除</button>
+					<button>4</button>
+					<button>5</button>
+					<button>6</button>
+					<button>清空</button>
+					<button>7</button>
+					<button>8</button>
+					<button>9</button>
+					<button>OK</button>
+					<button>0</button>
+					<button>.</button>
+				</div>
+			</NumberPadSection>
 		</Layout>
 	);
 }
