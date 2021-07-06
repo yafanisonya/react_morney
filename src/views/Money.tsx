4798ee5ddcd34a77b0b257c9afb73c2da87db30f
fanison <308955874@qq.com>
2021-07-06 10:50:56 +0800

    数字面板ui完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -69,7 +69,61 @@ const CategorySection = styled.section`
 	}
 `;
 const NumberPadSection = styled.section`
-
+	display:flex;
+	flex-direction:column;
+	> .output{
+		padding:0 16px;
+		text-align:right;
+		font-size:36px;
+		line-height:72px;
+		background:white;
+		box-shadow:inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+							 inset 0 5px 5px -5px rgba(0,0,0,0.25);
+	}
+	> .pad{
+		> button{
+			float:left;
+			border:none;
+			width:25%;
+			height:64px;
+			font-size:18px;
+			&.ok{
+				float:right;
+				height:128px;
+			}
+			&.zero{
+				width:50%;
+			}
+			&:nth-child(1){
+				background:#f2f2f2;
+			}
+			&:nth-child(2),
+			&:nth-child(5){
+				background:#E0E0E0;
+			}
+			&:nth-child(3),
+			&:nth-child(6),
+			&:nth-child(9){
+				background:#D3D3D3;
+			}
+			&:nth-child(4),
+			&:nth-child(7),
+			&:nth-child(10){
+				background:#C1C1C1;
+			}
+			&:nth-child(8),
+			&:nth-child(11),
+			&:nth-child(13){
+				background:#B8B8B8;
+			}
+			&:nth-child(12){
+				background:#9A9A9A;
+			}
+			&:nth-child(14){
+				background:#A9A9A9;
+			}
+		}
+	}
 `;
 
 function Money() {
@@ -97,8 +151,8 @@ function Money() {
 				</ul>
 			</CategorySection>
 			<NumberPadSection>
-				<div>1000</div>
-				<div>
+				<div className="output">1000</div>
+				<div className="pad clearfix">
 					<button>1</button>
 					<button>2</button>
 					<button>3</button>
@@ -110,8 +164,8 @@ function Money() {
 					<button>7</button>
 					<button>8</button>
 					<button>9</button>
-					<button>OK</button>
-					<button>0</button>
+					<button className="ok">OK</button>
+					<button className="zero">0</button>
 					<button>.</button>
 				</div>
 			</NumberPadSection>
