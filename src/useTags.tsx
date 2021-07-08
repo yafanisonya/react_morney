5ddec557a5eed15c0a159451ed30b8b572cc011b
fanison <308955874@qq.com>
2021-07-08 17:11:35 +0800

    return 优化代码

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,29 +11,13 @@ const defaultTags = [
 const useTags = ()=>{
   const [tags,setTags] = useState<{id:number;name:string}[]>(defaultTags);
   const findTag = (id:number) => tags.filter(tag => tag.id === id)[0];
-  const findTagIndex = (id:number)=>{
-    let result = -1;
-    for(let i = 0; i < tags.length; i++){
-      if(tags[i].id === id){
-        result = i;
-        break;
-      }
-    }
-    return result;
-  }
 
-  const updateTag = (id:number, obj: {name: string}) =>{
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index,1,{id:id,name: obj.name});
-    setTags(tagsClone);
+  const updateTag = (id:number, {name}: {name: string}) =>{
+    setTags(tags.map(tag => tag.id === id ? {id,name:name} : tag));
   }
 
   const deleteTag = (id:number) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index,1);
-    setTags(tagsClone);
+    setTags(tags.filter(tag => tag.id !== id));
   }　
 
   return {tags,setTags,findTag,updateTag,deleteTag};
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useTags} from 'useTags';
-import { useParams } from 'react-router';
+import { useParams, useHistory} from 'react-router';
 import Layout from 'components/Layout';
 import Icon from 'components/Icon';
 import {Button} from 'components/Button';
@@ -44,10 +44,15 @@ const Tag: React.FC = () =>{
       </Center>
     </div>
   );
+
+  const history = useHistory()
+  const onClickBack = ()=>{
+    history.goBack()
+  };
   return (
     <Layout>
       <Topbar>
-        <Icon name="left"/>
+        <Icon name="left" onClick={onClickBack}/>
         <span>编辑标签</span>
         <Icon/>
       </Topbar>
