9404ad09128025b3efaf27d91d89b3a839b4c0e6
fanison <308955874@qq.com>
2021-07-08 19:17:18 +0800

    tag存储到 localstorage

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,26 +1,44 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {createId} from 'lib/createId';
-
-const defaultTags = [
-  {id:createId(),name:'衣'},
-  {id:createId(),name:'食'},
-  {id:createId(),name:'住'},
-  {id:createId(),name:'行'}
-]
+import {useUpdate} from 'hooks/useUpdate';
 
 const useTags = ()=>{
-  const [tags,setTags] = useState<{id:number;name:string}[]>(defaultTags);
+  const [tags,setTags] = useState<{id:number;name:string}[]>([]);
+  useEffect(()=>{
+    let localTags = JSON.parse(window.localStorage.getItem('tags') || '[]');
+    if(localTags.length === 0){
+      localTags = [
+        {id:createId(),name:'衣'},
+        {id:createId(),name:'食'},
+        {id:createId(),name:'住'},
+        {id:createId(),name:'行'}
+      ];
+    }
+    setTags(localTags);
+  },[]);
+
+  useUpdate(()=>{
+    window.localStorage.setItem('tags',JSON.stringify(tags));
+  },[tags]);
+
   const findTag = (id:number) => tags.filter(tag => tag.id === id)[0];
 
   const updateTag = (id:number, {name}: {name: string}) =>{
     setTags(tags.map(tag => tag.id === id ? {id,name:name} : tag));
-  }
+  };
 
   const deleteTag = (id:number) => {
     setTags(tags.filter(tag => tag.id !== id));
-  }　
+  };　
 
-  return {tags,setTags,findTag,updateTag,deleteTag};
+  const addTag = ()=>{
+    const tagName = window.prompt('新增的名称为：');
+    if(tagName !== null && tagName !== ''){
+      setTags([...tags,{id: createId(), name: tagName}]);
+    }
+  }
+
+  return {tags,addTag,setTags,findTag,updateTag,deleteTag};
 };
 
 export {useTags};
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -25,7 +25,7 @@ const TagList = styled.ol`
 `;
 
 function Tags() {
-	const {tags} = useTags();
+	const {tags,addTag} = useTags();
 	return (
 		<Layout>
 			<TagList>
@@ -40,7 +40,7 @@ function Tags() {
 			</TagList>
 			<Center>
 				<Space/>
-				<Button>新增标签</Button>
+				<Button onClick={addTag}>新增标签</Button>
 			</Center>
 		</Layout>
 	);
