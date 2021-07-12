f435cd1cc8a89ecadaa79b2709d52fcdb81fb7c2
fanison <308955874@qq.com>
2021-07-12 23:05:50 +0800

    按日期分类排序展示

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,8 +1,8 @@
 import Layout from '../components/Layout';
-import React,{useState} from 'react';
+import React,{useState, ReactNode} from 'react';
 import {CategorySection} from './Money/CategorySection';
 import styled from 'styled-components';
-import {useRecords} from '../hooks/useRecords';
+import {useRecords, RecordItem} from '../hooks/useRecords';
 import {useTags} from '../hooks/useTags';
 import day from 'dayjs';
 
@@ -24,28 +24,62 @@ const Item = styled.div`
 	}
 `;
 
+const Header = styled.div`
+	padding: 10px 16px;
+	font-size: 18px;
+	line-height: 20px;
+`;
+
+
 function Statistics() {
 	const [category,setCategory] = useState<'-' | '+'>('-');
 	const {records} = useRecords();
 	const {getName} = useTags();
+	const hash : {[K:string]: RecordItem[]} = {};
+	const selectedRecords = records.filter(r=> r.category === category);
+
+	selectedRecords.map(r=>{
+		const key = day(r.createdAt).format('YYYY-MM-DD');
+		if(!(key in hash)){
+			hash[key] = [];
+		}
+		hash[key].push(r);
+	});
+
+	const array = Object.entries(hash).sort((a,b)=>{
+		if(a[0] === b[0]) return 0;
+		if(a[0] > b[0]) return -1;
+		if(a[0] < b[0]) return 1;
+		return 0;
+	})
+
 	return (
 		<Layout>
 			<CategoryWrapper>
 				<CategorySection value={category} onChange={value => setCategory(value)}/>
 			</CategoryWrapper>
+			
+			{array.map(([date,records])=>
+				<div>
+					<Header>
+						{date}
+					</Header>
+					<div>
+						{records.map(r=>{
+							return <Item>
+								<div className="tags oneLine">
+									{r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
+								</div>
+								{r.note && <div className="note">{r.note}</div>}
+								<div className="amount">￥{r.amount}</div>
+							</Item>
+						})}
+					</div>
+				</div>
+			)}
 
-			<div>
-				{records.map(r=>{
-					return <Item>
-						<div className="tags">
-							{r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
-						</div>
-						{r.note && <div className="note">{r.note}</div>}
-						<div className="amount">￥{r.amount}</div>
-						{/* {day(r.createdAt).format('YYYY年MM月DD日')} */}
-					</Item>
-				})}
-			</div>
+
+
 		</Layout>
 	);
 }
